5c2f0dd2a5daa1ff91f585c3982211cb0bb2f8a9
Amira Mohamed <a.mohamed@penta-b.com>
2021-07-21 20:33:02 +0200

    enhancement

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Card from '../UI/Card'
 import classes from './UserList.module.css'
 const UsersList = (props) => {
+    const { users } = props;
+    if (users.length === 0) return null;
     return (
+
         <Card className={classes.users}>
-        <ul>
-            {props.users.map((user) =>(
-                <li>{user.name} {user.age} years old</li>
-            ))}
-            
-        </ul>
+            <ul>
+                {users.map((user) => (
+                    <li>{user.name} {user.age} years old</li>
+                ))}
+
+            </ul>
         </Card>
+
+
     );
 };
 
